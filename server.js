211fdce0beddfcccc2aeaa8983762957c7f8fb8c
hectorbm94 <hectorbm94@gmail.com>
2016-01-09 18:47:06 +0100

    commit fatima7

--- server.js
+++ server.js
@@ -41,15 +41,15 @@ app.get('/:trackname', function(req, res) {
 app.post('/', upload.single('file'), function (req, res, next) {
 
     console.log(req.file); //form files
-    /*fse.move(req.file.path, '../mnt/nas/' + req.file.originalname + '.mp3', function(err){
-	if (err) return console.error(err);
-	console.log('success');
-    });*/
-    console.log(req.file.path);
-    fse.rename(req.file.path, 'public/' + req.file.originalname + '.mp3', function(err){
+    fse.move(req.file.path, '../mnt/nas/' + req.file.originalname + '.mp3', function(err){
 	if (err) return console.error(err);
 	console.log('success');
     });
+    console.log(req.file.path);
+    /*fse.rename(req.file.path, 'public/' + req.file.originalname + '.mp3', function(err){
+	if (err) return console.error(err);
+	console.log('success');
+    });*/
     res.send(200);
 })
 
